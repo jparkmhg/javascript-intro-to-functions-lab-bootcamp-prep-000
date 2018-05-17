fc6389ba6a602795ed1ac4021e8a30555c942b66
Judy Park <jparkmhg@gmail.com>
2018-05-17 07:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ return string.toUpperCase()
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(HELLO) {
+function logShout(string) {
   console.log(`HELLO.toUpperCaseCase`)
 }
